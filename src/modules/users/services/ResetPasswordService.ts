7609ff79735f32e99a82c19e750b6c21baa4d302
Jairo Koning <jairo.koning@gmail.com>
2020-08-10 21:21:09 -0300

    Add Ethereal for use email in development

--- src/modules/users/services/ResetPasswordService.ts
+++ src/modules/users/services/ResetPasswordService.ts
@@ -17,7 +17,7 @@ class ResetPasswordService {
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
 
-    @inject('UserTokensRepusitory')
+    @inject('UserTokensRepository')
     private userTokensRepository: IUserTokensRepository,
 
     @inject('HashProvider')
