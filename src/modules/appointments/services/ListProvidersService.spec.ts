9f26dc42e82b8e25e9b46090b746a119ee089d31
Jairo Koning <jairo.koning@gmail.com>
2020-08-12 21:14:11 -0300

    List all Providers

---
+++ src/modules/appointments/services/ListProvidersService.spec.ts
@@ -0,0 +1,47 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import ListProvidersService from './ListProvidersService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let listProviders: ListProvidersService;
+
+describe('ListProviders', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+
+    listProviders = new ListProvidersService(fakeUsersRepository);
+  });
+
+  it('should be able to list all providers', async () => {
+    const loggedUser = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    const user1 = await fakeUsersRepository.create({
+      name: 'Mary Paris',
+      email: 'maryparis@example.com',
+      password: '123456',
+    });
+
+    const user2 = await fakeUsersRepository.create({
+      name: 'Walt Green',
+      email: 'waltgreen@example.com',
+      password: '123456',
+    });
+
+    const user3 = await fakeUsersRepository.create({
+      name: 'Milenna Frank',
+      email: 'milennafrank@example.com',
+      password: '123456',
+    });
+
+    const providers = await listProviders.execute({
+      user_id: loggedUser.id,
+    });
+
+    expect(providers).toEqual([user1, user2, user3]);
+  });
+});
