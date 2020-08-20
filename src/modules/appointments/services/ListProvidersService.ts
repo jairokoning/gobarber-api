c5808e2144945753142f27220c56d3db4dca9378
Jairo Koning <jairo.koning@gmail.com>
2020-08-20 05:40:05 -0300

    Invalidate cache

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -29,7 +29,6 @@ class ListProvidersService {
         except_user_id: user_id,
       });
 
-      console.log('consulta feita');
       await this.cacheProvider.save(`providers-list:${user_id}`, users);
     }
 
