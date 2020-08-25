3fc67c8b0663f5419f7b2a5b12b75fe25bdcf98b
Jairo Koning <jairo.koning@gmail.com>
2020-08-25 05:53:31 -0300

    Appointments customers

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -62,6 +62,7 @@ class AppointmentsRepository implements IAppontmentsRepository {
             `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
         ),
       },
+      relations: ['user'],
     });
 
     return appointments;
